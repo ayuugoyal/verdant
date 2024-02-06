b781c757fab87870475c7ce1fc82bdd7a28c6ac8
ayuugoyal <ayushgoyal8178@gmail.com>
2024-02-07 05:11:38 +0530

    |FEAT| - add login signup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="flex flex-col gap-4 justify-center items-center h-screen">
+      <div className="text-center">WELCOME VERDANT</div>
+      <div className="flex gap-4">
+        <Link href={"/sign-up"}>
+          <Button>Sign Up</Button>
+        </Link>
+        <Link href={"/sign-in"}>
+          <Button>Log in</Button>
+        </Link>
+      </div>
     </div>
   );
 }
