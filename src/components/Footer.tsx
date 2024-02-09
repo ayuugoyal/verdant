a2e7986fec05462a15f87a72d020c8326b17c895
ayuugoyal <ayushgoyal8178@gmail.com>
2024-02-09 22:57:34 +0530

    final

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,9 +18,11 @@ const Footer = ({ resId }: { resId: string }) => {
           <path d="M946.5 505L560.1 118.8l-25.9-25.9a31.5 31.5 0 0 0-44.4 0L77.5 505a63.9 63.9 0 0 0-18.8 46c.4 35.2 29.7 63.3 64.9 63.3h42.5V940h691.8V614.3h43.4c17.1 0 33.2-6.7 45.3-18.8a63.6 63.6 0 0 0 18.7-45.3c0-17-6.7-33.1-18.8-45.2zM568 868H456V664h112v204zm217.9-325.7V868H632V640c0-22.1-17.9-40-40-40H432c-22.1 0-40 17.9-40 40v228H238.1V542.3h-96l370-369.7 23.1 23.1L882 542.3h-96.1z" />
         </svg>
       </button>
-      <button className="bg-transparent flex items-center justify-center  transition-all duration-[ease-in-out] delay-[0.3s] cursor-pointer rounded-[50%] border-0 hover:translate-y-[-3px]">
-        <IoHome width={15} height={15} />
-      </button>
+      <Link href={`/mainApp/${resId}/`}>
+        <button className="bg-transparent flex items-center justify-center  transition-all duration-[ease-in-out] delay-[0.3s] cursor-pointer rounded-[50%] border-0 hover:translate-y-[-3px]">
+          <IoHome width={15} height={15} />
+        </button>
+      </Link>
       <Link href={`/mainApp/${resId}/verdesocial`}>
         <button className="w-10 h-10 bg-transparent flex items-center justify-center  transition-all duration-[ease-in-out] delay-[0.3s] cursor-pointer rounded-[50%] border-0 hover:translate-y-[-3px]">
           <svg
